5a5428a49958252e7dc118db366f850a33aaa416
Fathihah <d032310420@student.utem.edu.my>
2025-11-19 22:38:51 +0800

    database connect

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<?php
+//database connection variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "planwise_db";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
