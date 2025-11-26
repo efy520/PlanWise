0034c92d29d83aebada862e9bea766499a6e8ded
Fathihah <d032310420@student.utem.edu.my>
2025-11-26 18:43:39 +0800

    check db connection

--- db_connection.php
+++ db_connection.php
@@ -12,4 +12,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+else {
+    // Uncomment the line below for debugging purposes
+     echo "Connected successfully";
+}
 ?>
